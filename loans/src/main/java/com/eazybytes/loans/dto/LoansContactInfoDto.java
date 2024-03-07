ba75edd453639f776d00060682a953b91593af6c
Saurav Kumar <shah.kumar.saurav@gmail.com>
2024-03-07 11:42:21 +0530

    update props in git repo and use refresh endponts of the actuator to update the props values in all 3 ms

--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,20 @@
 package com.eazybytes.loans.dto;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansContactInfoDto {
+
+	private String message;
+	private Map<String, String> contactDetails;
+	private List<String> onCallSupport;
+
 }
