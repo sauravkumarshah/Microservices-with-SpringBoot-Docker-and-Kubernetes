f8ecf5911587dee106891c95934037cce906633d
Saurav Kumar <shah.kumar.saurav@gmail.com>
2024-03-09 18:30:00 +0530

    added retry attempts only for TimeoutExceptions

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/ResponseTraceFilter.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/ResponseTraceFilter.java
@@ -7,25 +7,30 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class ResponseTraceFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
+	private static final Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
 
-    @Autowired
-    FilterUtility filterUtility;
+	@Autowired
+	FilterUtility filterUtility;
 
-    @Bean
-    public GlobalFilter postGlobalFilter() {
-        return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
-            }));
-        };
-    }
+	@Bean
+	public GlobalFilter postGlobalFilter() {
+		return (exchange, chain) -> {
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+				String correlationId = filterUtility.getCorrelationId(requestHeaders);
+
+				if (exchange.getResponse().getHeaders().containsKey(filterUtility.CORRELATION_ID)) {
+					logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+					exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
+				}
+
+			}));
+		};
+	}
 }
