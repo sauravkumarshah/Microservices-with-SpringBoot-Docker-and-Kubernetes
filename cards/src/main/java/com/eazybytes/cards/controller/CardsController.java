7e7f65acb43bbba6f09886ca1e1d8f049816ea33
Saurav Kumar <shah.kumar.saurav@gmail.com>
2024-03-12 21:30:02 +0530

    distributed tracing with open telemetry

--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -79,8 +79,10 @@ public class CardsController {
 	@GetMapping("/fetch")
 	public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
 			@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
-		logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+
+		logger.debug("fetchCardDetails method start");
 		CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+		logger.debug("fetchCardDetails method end");
 		return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
 	}
 
